ec2d65538236330be7a39cffa62466ae47e60433
zalfadisha <nurdiyansah@gmail.com>
2018-09-10 22:25:15 +0700

    Update JS_This.js

--- JS_This.js
+++ JS_This.js
@@ -10,6 +10,11 @@
   };
 
   goat.diet(); // Print Herbivore
+/*
+The this keyword references the calling object which provides access to the calling object's properties.
+In the example above, the calling object is goat and by using this we're accessing the goat object itself,
+and then the dietType property of goat by using property dot notation. 
+*/
 
 
 const goat = {
