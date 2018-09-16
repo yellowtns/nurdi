9abcb00aeebb50a4a6861f9fa3a2fa37c2d7cad2
zalfa <nurdiyansah@gmail.com>
2018-09-16 21:56:32 +0700

    Create getterSetter.js

---
+++ getterSetter.js
@@ -0,0 +1,27 @@
+const team = {
+  _players: [
+    				 {firstName: "Pablo",lastName:"Sanchez",age: 11},
+             {firstName: "Lionel",lastName:"Messi",age: 21},
+             {firstName: "Mohammad",lastName:"Salah",age: 20}            
+  ],
+  _games : [
+  					{opponent: "Broncos", teamPoints: 42, opponentPoints: 27},
+    				{opponent: "Ronaldo", teamPoints: 41, opponentPoints: 30},
+    				{opponent: "Aguero", teamPoints: 40, opponentPoints: 29}
+  ],
+
+get player : () {
+    return this._players;
+		}
+  
+};
+console.log(team.players);
+
+/*
+player : function () {
+    return this._players;
+		}
+  
+console.log(team.players());
+
+*/
