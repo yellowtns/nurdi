56c9f38ee9e0f9bb7e64dc584909e07ef53401d5
zalfa <nurdiyansah@gmail.com>
2018-09-17 21:40:05 +0700

    Create methods.js

---
+++ methods.js
@@ -0,0 +1,11 @@
+//Methods
+
+//Methods are nothing more than properties that hold function values. This is a simple method:
+
+let rabbit = {};
+rabbit.speak = function(line) {
+  console.log(`The rabbit says '${line}'`);
+};
+
+rabbit.speak("I'm alive.");
+// → The rabbit says 'I'm alive.'
