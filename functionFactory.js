674a8e17073ffab2b9638e94deca8bdbf204d774
zalfa <nurdiyansah@gmail.com>
2018-09-17 20:10:21 +0700

    Create functionFactory.js

---
+++ functionFactory.js
@@ -0,0 +1,10 @@
+//When a function returns an object, we call it a factory function.
+//Let’s take a look at a simple example.
+
+function createJelly() {
+  return {
+    type: 'jelly',
+    colour: 'red'
+    scoops: 3
+  };
+}
