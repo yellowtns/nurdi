4463be2771b7949d5a4239b180d882846b1763a8
zalfa <nurdiyansah@gmail.com>
2018-09-17 21:44:25 +0700

    Create class.js

---
+++ class.js
@@ -0,0 +1,10 @@
+//class
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+//class instances
+const surgeonCurry = new Surgeon ('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon ('Durant', 'Orthopedics');
