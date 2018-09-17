cdb101d4a6eed77440f1c6f81b0c21669eb8a37f
zalfa <nurdiyansah@gmail.com>
2018-09-17 20:15:58 +0700

    Update closure.js

--- closure.js
+++ closure.js
@@ -8,6 +8,10 @@ What happens to local bindings when the function call that created them is no lo
 The following code shows an example of this. It defines a function, wrapValue, 
 that creates a local binding. It then returns a function that accesses and returns this local binding.
 
+being able to reference a specific instance of a local binding in an enclosing scope—is called closure. 
+A function that references bindings from local scopes around it is called a closure. 
+This behavior not only frees you from having to worry about lifetimes of bindings 
+but also makes it possible to use function values in some creative ways.
 */
 
 function wrapValue(n) {
@@ -21,3 +25,12 @@ console.log(wrap1());
 // → 1
 console.log(wrap2());
 // → 2
+
+
+function multiplier(factor) {
+  return number => number * factor;
+}
+
+let twice = multiplier(2);
+console.log(twice(5));
+// → 10
