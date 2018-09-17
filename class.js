4b2e3fe73a08aba6b80cc337cdc4b22be4103f6d
zalfa <nurdiyansah@gmail.com>
2018-09-17 22:02:01 +0700

    Update class.js

--- class.js
+++ class.js
@@ -1,10 +1,26 @@
-//class
 class Surgeon {
   constructor(name, department) {
-    this.name = name;
-    this.department = department;
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get department() {
+    return this._department;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
   }
 }
-//class instances
-const surgeonCurry = new Surgeon ('Curry', 'Cardiovascular');
-const surgeonDurant = new Surgeon ('Durant', 'Orthopedics');
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
